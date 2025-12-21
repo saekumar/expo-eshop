acb4667b82f8271a62d8071d27191fa82f94f6e3
saikumar <saikumarpuppala249@gmail.com>
2025-12-21 16:02:03 +0530

    Set up backend environment with dotenv, add CORS support, and update package files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,18 @@
-import express from "express"
-
+import express from 'express'
+import path from 'path'
+import { ENV } from './config/env.js'
+import cors from 'cors'
 
 const app = express()
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://your-admin.vercel.app'],
+    credentials: true,
+  })
+)
 
-app.get('/api/check',(req,res)=> res.status(200).json({message:"success"}))
+app.get('/api/check', (req, res) =>
+  res.status(200).json({ message: 'success' })
+)
 
-app.listen(3000,()=>console.log('server is up and running'))
+app.listen(ENV.PORT, () => console.log('server is up and running'))
